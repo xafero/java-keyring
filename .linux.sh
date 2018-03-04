3e1f9420858d866c85cf2bb983039112f940ad9e
Rex <rex.hoffman@salesforce.com>
2018-03-04 12:56:39 -0800

    gnome keyring info for export

--- .linux.sh
+++ .linux.sh
@@ -2,7 +2,11 @@
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then 
   eval $(/usr/bin/dbus-launch --sh-syntax); 
-  echo "password" | gnome-keyring-daemon --unlock;
+  export `/usr/bin/gnome-keyring-daemon`
   echo DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS;
   echo DBUS_SESSION_BUS_PID=$DBUS_SESSION_BUS_PID;
+  echo GNOME_KEYRING_CONTROL=$GNOME_KEYRING_CONTROL;
+  echo SSH_AUTH_SOCK=$SSH_AUTH_SOCK;
+  echo GPG_AGENT_INFO=$GPG_AGENT_INFO;
+  echo GNOME_KEYRING_PID=$GNOME_KEYRING_PID;
 fi 
