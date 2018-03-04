8f2cc8bccbc8f3d8df0cdfb03570c7b179ec255b
Rex <rex.hoffman@salesforce.com>
2018-03-04 12:38:49 -0800

    keyring password & typo

--- .linux.sh
+++ .linux.sh
@@ -2,7 +2,7 @@
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then 
   eval $(/usr/bin/dbus-launch --sh-syntax); 
-  gnome-keyring-daemon with --unlock;
-  echo DBUS_SESSION_BUS_ADDRES=$DBUS_SESSION_BUS_ADDRES;
+  echo "password" | gnome-keyring-daemon --unlock;
+  echo DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS;
   echo DBUS_SESSION_BUS_PID=$DBUS_SESSION_BUS_PID;
 fi 
