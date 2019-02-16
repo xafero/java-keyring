db1007f3e74d760b6adfc97d7c06f700a0101102
Rex Hoffman <rex@e-hoffman.org>
2019-02-16 00:42:21 -0800

    use latest parent and clean up licenses.

--- java-keyring/src/main/java/net/east301/keyring/windows/PasswordEntry.java
+++ java-keyring/src/main/java/net/east301/keyring/windows/PasswordEntry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017, Saleforce.com, Inc
+ * Copyright © 2017, Rex Hoffman
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,7 +24,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package net.east301.keyring.windows;
 
 import java.io.Serializable;
