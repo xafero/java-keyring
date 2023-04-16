536c278e58b43214c4f5d853da48f7261177967a
Rex Hoffman <rex@e-hoffman.org>
2023-04-15 20:18:44 -0700

    f

--- .setupkeystore.sh
+++ .setupkeystore.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 set -x
-if [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then 
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then 
   export $(dbus-launch)
   eval "$(echo '\n' | gnome-keyring-daemon --unlock)"
   echo xxx@gmail.com | secret-tool store --label="main" email address
   PASS=$(secret-tool lookup email address)
 
   echo Checking the secret can be retrieved.
-  if  [[ "$PASS" != "xxx@gmail.com" ]]; then
+  if  [ "$PASS" != "xxx@gmail.com" ]; then
     echo FAIL: the secret could not be retrieved.
     return 1;
   fi
   echo SUCCESS: the secret could be retrieved.
 fi 
 
-if [[ "$(uname -s)" == "Darwin" ]]; then 
+if [ "$(uname -s)" == "Darwin" ]; then 
   ls -la ~/Library/Keychains/
   #rm -rf ~/Library/Keychains/login.keychain
 fi
